501a72bf6c3d5bfc7082ff92592484b2b724b3e2
hamza naseer <hamza.naseer@vigorant.com>
2022-08-29 19:15:40 +0500

    updated projects and resume

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './Portfolio.css'
 import IMG1 from '../../assets/portfolio1.jpg'
-import IMG2 from '../../assets/portfolio2.jpg'
+import IMG2 from '../../assets/dental.png'
 import IMG3 from '../../assets/portfolio3.jpg'
 import IMG4 from '../../assets/portfolio4.jpg'
 import IMG5 from '../../assets/portfolio5.png'
@@ -15,8 +15,8 @@ const Portfolio = () => {
       <h5>My Recent Work</h5>
       <h2>Portfolio</h2>
       <div className="container portfolio__container">
-        <PortfolioArticle img={IMG1} title='CryptoGeek' github='https://github.com/HamzaaNaseer/crypto-app' demo='https://hamzacod3edcryptoapp.netlify.app/' />
-        <PortfolioArticle img={IMG2} title='my title' github='https://github.com/hamzaaNaseer' demo='https://github.com/hamzaaNaseer' />
+        <PortfolioArticle img='https://images.unsplash.com/photo-1516245834210-c4c142787335?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2069&q=80' title='CryptoGeek' github='https://github.com/HamzaaNaseer/crypto-app' demo='https://hamzacod3edcryptoapp.netlify.app/' />
+        <PortfolioArticle img={IMG2} title='Dental Implants Frontend' github='https://github.com/hamzaaNaseer/dental-implants' demo='https://dental-implants-99ls75r3y-hamzaanaseer.vercel.app/' />
         <PortfolioArticle img={IMG3} title='my title' github='https://github.com/hamzaaNaseer' demo='https://github.com/hamzaaNaseer' />
         <PortfolioArticle img={IMG4} title='my title' github='https://github.com/hamzaaNaseer' demo='https://github.com/hamzaaNaseer' />
         <PortfolioArticle img={IMG5} title='my title' github='https://github.com/hamzaaNaseer' demo='https://github.com/hamzaaNaseer' />
